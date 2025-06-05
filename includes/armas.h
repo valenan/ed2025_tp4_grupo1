5e7e9115a75effa13d2e23dfb293e14e098e1e9b
slendertroll777 <titosantinoagustin@gmail.com>
2025-06-05 14:49:05 -0300

    Update armas.h

--- includes/armas.h
+++ includes/armas.h
@@ -1,9 +1,7 @@
 #ifndef ARMAS_H
 #define ARMAS_H
 
-
 #include <iostream>
-
 using namespace std;
 
 class arma {
@@ -14,29 +12,27 @@ private:
     bool contundente;
     bool veneno;
 public:
-
-    arma(string nombre, int dano, string descripcion,bool contundente,bool veneno);
+    arma(string nombre, int dano, string descripcion, bool contundente, bool veneno);
     void mostrar_arma();
     int get_dano();
     bool get_veneno();
     bool get_contundente();
-
 };
-class baston: public arma{
+
+class baston : public arma {
 private:
     int especial;
-    int cooldown;               
+    int cooldown;
 public:
-baston(int especial, int cooldown,string nombre, int dano, string descripcion,bool contundente,bool veneno);
-/*los bastones de mago tienen habilidades especiales:
-0=Confusion: Produce que el personaje atacado ataque a sus compañeros
-1=Curacion: Cura tus personajes
-2=Necromacia: Trae un personaje de la muerte durante el siguiente turno
-todos los especiales consumen un turno
-*/
-void mostrar_arma();
-int get_especial();
-int get_cooldown();
+    baston(int especial, int cooldown, string nombre, int dano, string descripcion, bool contundente, bool veneno);
+    /*
+    0 = Confusión: el enemigo ataca a su equipo
+    1 = Curación: cura a un aliado
+    2 = Necromancia: revive un aliado 1 turno
+    */
+    void mostrar_arma();
+    int get_especial();
+    int get_cooldown();
 };
 
 #endif
